4d02ed0af2458b078856ed585da7ba87af61400c
yiranshu <shuyiran1425@hotmail.com>
2024-05-16 11:21:02 -0400

    serializer

--- server/models.py
+++ server/models.py
@@ -10,8 +10,8 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
-
-class Doctor(db.Model):
+#SerializerMixin does to_dict
+class Doctor(db.Model, SerializerMixin):
     
     __tablename__ = 'doctors_table'
 
@@ -19,18 +19,46 @@ class Doctor(db.Model):
     name = db.Column(db.String, nullable=False)
     specialty = db.Column(db.String)
 
+    appointments = db.relationship('Appointment', back_populates = 'doctor')
+    patients = association_proxy('appointments', 'patient')
 
-class Patient(db.Model):
+    serialize_rules = ['-appointments','patients','-patients.doctors']
+    # serialize_rules = ['-appointments.doctors','-patients']
+ 
+    #converts the class instance to an object, so readable to json
+    # def to_dict(self):
+    #     return{
+
+    #     }
+
+
+
+#plug in, inherit, can do multiple parents
+class Patient(db.Model, SerializerMixin):
     
     __tablename__ = 'patients_table'
     
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
 
+    appointments = db.relationship('Appointment', back_populates = 'patient')
+    doctors = association_proxy('Appointments', 'doctor')
 
-class Appointment(db.Model):
+    serialize_rules = ['-appointments','doctors','-doctors.patients']
+    # serialize_rules = ['-appointments.patients','-doctors']
+
+
+class Appointment(db.Model, SerializerMixin):
     
     __tablename__ = 'appointments_table'
 
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.String)
+
+    patient_id = db.Column(db.Integer, db.ForeignKey('patients_table.id'))
+    doctor_id = db.Column(db.Integer, db.ForeignKey('doctors_table.id'))
+
+    patient = db.relationship('Patient', back_populates='appointments')
+    doctor = db.relationship('Doctor', back_populates='appointments')
+
+    serialize_rules = ['-doctor.appointments', '-patient.appointments']
